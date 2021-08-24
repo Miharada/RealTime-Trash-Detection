cb36f83245633369364de1ce03787b6edcf79790
Cal <mpascaldewantara@gmail.com>
2021-08-24 23:08:42 +0700

    Coba apakah OpenCV ter load atau ngga

--- RealClean/app/src/main/java/com/example/realclean/MainActivity.kt
+++ RealClean/app/src/main/java/com/example/realclean/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.realclean
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.realclean.databinding.ActivityMainBinding
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        init {
+            if (OpenCVLoader.initDebug()){
+                Log.d("WOOOOOY", "SUKSESSSSSSSSSS")
+            }else{
+                Log.d("WOOOOOY", "GAGAAALLLLLL")
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOpenCam.setOnClickListener {
+
+        }
     }
 }
